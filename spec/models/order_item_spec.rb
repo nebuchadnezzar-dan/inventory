4ee0d1b1ea97921886222e4d19d469486c627717
nebuchadnezzar-dan <danielbanasen@gmail.com>
2020-02-08 22:11:45 +0800

    Added the correct order item, validations and associations

---
+++ spec/models/order_item_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe OrderItem, type: :model do
+  describe 'validations' do
+    subject(:order_item) { build(:order_item) }
+
+    it { is_expected.to validate_numericality_of(:quantity).is_greater_than(0) }
+  end
+
+  describe 'associations' do
+    subject(:order_item) { build(:order_item) }
+
+    it { is_expected.to belong_to(:product) }
+    it { is_expected.to belong_to(:order) }
+  end
+end
