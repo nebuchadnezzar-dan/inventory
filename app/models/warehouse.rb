401b230136c9af9aeb21f5c21744d789b05bc83d
nebuchadnezzar-dan <danielbanasen@gmail.com>
2020-02-20 20:34:12 +0800

    Modified warehouse model

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,8 +1,12 @@
 class Warehouse < ApplicationRecord
-  has_many :stocks
+  has_many :stocks, dependent: :destroy
   has_many :products, through: :stocks
   has_many :orders
 
   validates :city, presence: true
   validates :province, presence: true
+
+  def full_address
+    "#{street}, #{city}, #{province}"
+  end
 end
