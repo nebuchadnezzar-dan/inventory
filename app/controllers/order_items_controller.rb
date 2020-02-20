025aac61c197d128f2a5010e411c116fef28e15e
nebuchadnezzar-dan <danielbanasen@gmail.com>
2020-02-20 22:15:20 +0800

    Added order-items controller

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,22 @@
+class OrderItemsController < ApplicationController
+  before_action :set_order
+
+  def create
+    @order_item = Order.new(order_items_params)
+
+    if @order_item.save
+      redirect_to order_path(@order)
+    end
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_items_params
+    params.require(:order_item).permit(:order_id, :product_id, :quantity)
+  end
+
+end
