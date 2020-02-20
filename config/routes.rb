4090c97842b6662368ac80c886860e550aaa9cec
nebuchadnezzar-dan <danielbanasen@gmail.com>
2020-02-20 22:19:23 +0800

    modified routes to add order_items

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@
 
 Rails.application.routes.draw do
   resources :products, only: %i[index show edit destroy show new create edit update]
-  resources :orders, only: %i[index show create new edit destroy update]
   resources :warehouses, only: %i[index show edit create new update destroy]
   resources :stocks
+
+  resources :orders do
+    resources :order_items, only: %i[create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
