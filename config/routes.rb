6383495e14b5ee020b9d7df1b8ebec4b036ee3bd
nebuchadnezzar-dan <danielbanasen@gmail.com>
2020-02-20 20:45:00 +0800

    Added routes for warehouse

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   resources :products, only: %i[index show edit destroy show new create edit update]
   resources :orders, only: %i[index show create new edit destroy update]
+  resources :warehouse, only: %i[index show create new update destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
