ca55577ea4997226285d0434c1a5bace157bd3cf
nebuchadnezzar-dan <danielbanasen@gmail.com>
2020-02-22 03:27:46 +0800

    Completed CRUD functionality for order_items

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :stocks
 
   resources :orders do
-    resources :order_items, only: %i[create]
+    resources :order_items, only: %i[create edit update destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
