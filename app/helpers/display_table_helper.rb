dffa378db7690e57032ade69fc7d324ccf54428d
nebuchadnezzar-dan <danielbanasen@gmail.com>
2020-02-23 19:41:46 +0800

    implemented the table partial on all indexes

--- app/helpers/display_table_helper.rb
+++ app/helpers/display_table_helper.rb
@@ -1,8 +1,9 @@
 module DisplayTableHelper
 
-  def map_over_records(record)
+  def map_over_records(record, title)
     record_hash = {}
-    (record.attributes.keys - ['id', 'created_at', 'updated_at']).each { |rec| record_hash[rec] = record[rec] }
+    record_hash['full_address'] = record.warehouse.full_address if title == 'order'
+    (record.attributes.keys - %w[id uuid created_at updated_at warehouse_id]).each { |rec| record_hash[rec] = record[rec] }
     record_hash
   end  
   
