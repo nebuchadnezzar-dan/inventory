d70dd734f0fb4fcf85ecfacae84fcebfcbe9c0d5
nebuchadnezzar-dan <danielbanasen@gmail.com>
2020-03-05 18:30:33 +0800

    displayed the table body of warehouse from ruby

--- app/javascript/components/Warehouse/Warehouse.jsx
+++ app/javascript/components/Warehouse/Warehouse.jsx
@@ -6,19 +6,25 @@ import DynamicTable from '../Table/DynamicTable'
 class Warehouse extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      products: _([...this.props.products, ...this.props.counts]).groupBy('id').map((g) => _.mergeWith({}, ...g, (obj, src) => _.isArray(obj) ? obj.concat(src) : undefined)).value()
+    }
   }
   render(){
-    const data = [...this.props.products, ...this.props.counts]
-    const mapped = _(data).groupBy('id').map((g) => _.mergeWith({}, ...g, (obj, src) => _.isArray(obj) ? obj.concat(src) : undefined)).value();
-    console.log(mapped)
+    // const data = [...this.props.products, ...this.props.counts]
+    // const mapped = _(data).groupBy('id').map((g) => _.mergeWith({}, ...g, (obj, src) => _.isArray(obj) ? obj.concat(src) : undefined)).value();
+    // console.log(mapped)
+    const tableBody = _.map(this.state.products, product => (
+      <tr key={product.id}>
+        <td>{product.name}</td>
+        <td>{product.count}</td>
+        <td>A</td>
+      </tr>
+    ))
    return (
     <>
-      <DynamicTable headers={['Product', 'Quantity', 'Actions']} hover={true} varaint="light" size="sm" headerColor="bg-primary">
-        <tr>
-          <td>A</td>
-          <td>B</td>
-          <td>C</td>
-        </tr>
+      <DynamicTable headers={['Product', 'Quantity', 'Actions']} hover={true} variant="light" size="sm" headerColor="bg-primary">
+        {tableBody}
       </DynamicTable>
     </>)
   }
